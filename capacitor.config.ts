e7242e8c7487f1a1d2c7a1e99c7483ea1a611a91
salaheddineva <salaheddine@veniseactivation.com>
2023-03-06 00:15:14 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Simple Ionic Project',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
